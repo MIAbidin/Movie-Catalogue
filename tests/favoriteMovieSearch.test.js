662c82313a0c1df430d411b3cca820ac310ceaba
MIAbidin <muhammadirfanabidin@gmail.com>
2024-05-23 20:33:13 +0700

    Jangan Bertele tele

--- tests/favoriteMovieSearch.test.js
+++ tests/favoriteMovieSearch.test.js
@@ -1,5 +1,6 @@
 import { spyOn } from 'jest-mock';
-import FavoriteMovieSearchPresenter from '../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter';
+import FavoriteMovieSearchPresenter
+  from '../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter';
 import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
  
 describe('Searching movies', () => {
@@ -8,10 +9,11 @@ describe('Searching movies', () => {
   const searchMovies = (query) => {
     const queryElement = document.getElementById('query');
     queryElement.value = query;
+ 
     queryElement.dispatchEvent(new Event('change'));
   };
  
-  beforeEach(() => {
+  const setMovieSearchContainer = () => {
     document.body.innerHTML = `
       <div id="movie-search-container">
         <input id="query" type="text">
@@ -21,9 +23,19 @@ describe('Searching movies', () => {
         </div>
       </div>
     `;
+  };
  
+  const constructPresenter = () => {
     spyOn(FavoriteMovieIdb, 'searchMovies');
-    presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
+ 
+    presenter = new FavoriteMovieSearchPresenter({
+      favoriteMovies: FavoriteMovieIdb,
+    });
+  };
+ 
+  beforeEach(() => {
+    setMovieSearchContainer();
+    constructPresenter();
   });
  
   it('should be able to capture the query typed by the user', () => {
@@ -36,5 +48,55 @@ describe('Searching movies', () => {
     searchMovies('film a');
  
     expect(FavoriteMovieIdb.searchMovies).toHaveBeenCalledWith('film a');
+  });
+ 
+  it('should show the found movies', () => {
+    presenter._showFoundMovies([{ id: 1 }]);
+    expect(document.querySelectorAll('.movie').length).toEqual(1);
+ 
+    presenter._showFoundMovies([
+      {
+        id: 1,
+        title: 'Satu',
+      },
+      {
+        id: 2,
+        title: 'Dua',
+      },
+    ]);
+    expect(document.querySelectorAll('.movie').length).toEqual(2);
+  });
+ 
+  it('should show the title of the found movies', () => {
+    presenter._showFoundMovies([
+      {
+        id: 1,
+        title: 'Satu',
+      },
+    ]);
+ 
+    expect(document.querySelectorAll('.movie__title').item(0).textContent).toEqual('Satu');
+ 
+    presenter._showFoundMovies([
+      {
+        id: 1,
+        title: 'Satu',
+      },
+      {
+        id: 2,
+        title: 'Dua',
+      },
+    ]);
+ 
+    const movieTitles = document.querySelectorAll('.movie__title');
+ 
+    expect(movieTitles.item(0).textContent).toEqual('Satu');
+    expect(movieTitles.item(1).textContent).toEqual('Dua');
+  });
+ 
+  it('should show - for found movie without title', () => {
+    presenter._showFoundMovies([{ id: 1 }]);
+ 
+    expect(document.querySelectorAll('.movie__title').item(0).textContent).toEqual('-');
   });
 });
