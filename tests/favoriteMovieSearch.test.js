d7bc7b94f29c5b139429c4b92b3d7fd1adb2ba05
MIAbidin <muhammadirfanabidin@gmail.com>
2024-05-24 09:32:14 +0700

    Ketika hampa

--- tests/favoriteMovieSearch.test.js
+++ tests/favoriteMovieSearch.test.js
@@ -162,4 +162,30 @@ describe('Searching movies', () => {
       expect(favoriteMovies.getAllMovies).toHaveBeenCalled();
     });
   });
+
+  describe('When no favorite movies could be found', () => {
+    it('should show the empty message', (done) => {
+      document
+        .getElementById('movie-search-container')
+        .addEventListener('movies:searched:updated', () => {
+          expect(document.querySelectorAll('.movies__not__found').length).toEqual(1);
+ 
+          done();
+        });
+ 
+      favoriteMovies.searchMovies.mockImplementation((query) => []);
+ 
+      searchMovies('film a');
+    });
+ 
+    it('should not show any movie', (done) => {
+      document.getElementById('movie-search-container')
+        .addEventListener('movies:searched:updated', () => {
+          expect(document.querySelectorAll('.movie').length).toEqual(0);
+          done();
+        });
+      favoriteMovies.searchMovies.mockImplementation((query) => []);
+      searchMovies('film a');
+    });
+  });
 });
